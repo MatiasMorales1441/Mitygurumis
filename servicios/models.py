206d258058bbc6680be01355dca13c306405cd7a
crosbostol <cristobal.oyarzun@cloud.uautonoma.cl>
2021-10-25 19:30:46 -0300

    ayuda

--- servicios/models.py
+++ servicios/models.py
@@ -4,11 +4,7 @@ from django.utils.html import format_html
 # Create your models here.
 class Servicio(models.Model):
     titulo = models.CharField(max_length=50, verbose_name="titulo")
-<<<<<<< HEAD
     contenido =models.TextField(max_length=1000, verbose_name='contenido')
-=======
-    contenido =models.CharField(max_length=200, verbose_name='contenido')
->>>>>>> d09a9454c6ae7f6bdc8008ff03d3854608b6079d
     imagen = models.ImageField(upload_to = 'servicios')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
