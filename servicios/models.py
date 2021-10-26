ce69f12fba7b411202b754e12e90627989a5f9b1
crosbostol <cristobal.oyarzun@cloud.uautonoma.cl>
2021-10-25 22:57:26 -0300

    cambio de modelos

--- servicios/models.py
+++ servicios/models.py
@@ -3,9 +3,13 @@ from sorl.thumbnail import get_thumbnail
 from django.utils.html import format_html
 # Create your models here.
 class Servicio(models.Model):
-    titulo = models.CharField(max_length=50, verbose_name="titulo")
+    titulo = models.CharField(max_length=50, verbose_name="titulo")  
+    nombre = models.CharField(max_length= 60)
     contenido =models.TextField(max_length=1000, verbose_name='contenido')
     imagen = models.ImageField(upload_to = 'servicios')
+    procedencia = models.CharField( max_length=60)
+
+    file = models.FileField(upload_to= 'servicios/files')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
   
